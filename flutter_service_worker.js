170135e3a901dc6a305b8a604e851b856128e28c
cassidy753 <78695649+cassidy753@users.noreply.github.com>
2026-07-22 14:46:13 +0800

    Clean rebuild - borderless design

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a9dfb9ae46e5351c0998aa5f95d5e8b4",
+const RESOURCES = {"flutter_bootstrap.js": "4335d5453035b9004b53d7d8629879fe",
 "ig_day3.html": "20823978f0b901f90e63520249dc8c69",
 "version.json": "69b60e0eb2d8616141b19af755c345bc",
 "index.html": "c2662dc30eaa1e70dad5d27d79017413",
