f562851cae72552a0ef1263274d85628a9802f6d
cassidy753 <78695649+cassidy753@users.noreply.github.com>
2026-07-23 21:38:37 +0800

    Revert font preload - restore splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'app.dart' as app;
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Preload Google Fonts for CanvasKit rendering
-  // This prevents Chinese text from rendering as blank/garbled in splash
-  await GoogleFonts.pendingFonts([
-    GoogleFonts.notoSansTc(textStyle: const TextStyle()),
-    GoogleFonts.notoSerifTc(textStyle: const TextStyle()),
-  ]);
-
   runApp(const app.TypingselfApp());
 }
