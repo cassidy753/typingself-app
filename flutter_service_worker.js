981166f13e63a59c6f40322397286ea7ea7b56eb
cassidy753 <78695649+cassidy753@users.noreply.github.com>
2026-07-24 08:58:41 +0800

    Fix type error

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "de5e3e08592917f4b9b81c0b0391866b",
+const RESOURCES = {"flutter_bootstrap.js": "30e174283073438b035165f50be20e56",
 "ig_day3.html": "20823978f0b901f90e63520249dc8c69",
 "version.json": "69b60e0eb2d8616141b19af755c345bc",
 "index.html": "c2662dc30eaa1e70dad5d27d79017413",
